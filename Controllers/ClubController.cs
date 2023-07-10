af3e9d4820dca63742850136996064f1902d6c69
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-07-10 16:12:23 +0200

    Controllers

---
+++ Controllers/ClubController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RunGroupWebApp.Controllers
+{
+	public class ClubController : Controller
+	{
+		//Index in the Controller => Index.cshtml in the View folder > Club > Index.cshtml
+		//it has to start with "Index" in the club folder if the IActionResult is set to Index()
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}

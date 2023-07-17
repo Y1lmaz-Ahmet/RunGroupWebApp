e09b885938419362d4cbe3016958793359e39125
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-07-17 21:22:52 +0200

    Edit functionality for ClubController

--- Repository/ClubRepository.cs
+++ Repository/ClubRepository.cs
@@ -37,6 +37,10 @@ namespace RunGroupWebApp.Repository
         {
             return await _context.Clubs.Include(x => x.Address).FirstOrDefaultAsync(i => i.Id == id);
         }
+        public async Task<Club> GetByIdAsyncNoTracking(int id)
+        {
+            return await _context.Clubs.Include(x => x.Address).AsNoTracking().FirstOrDefaultAsync(i => i.Id == id);
+        }
 
         public async Task<IEnumerable<Club>> GetClubByCity(string city)
         {

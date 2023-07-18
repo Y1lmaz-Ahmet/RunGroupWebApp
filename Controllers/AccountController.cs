1c84457ad66640f67b663d068f6dc4643a0fa53d
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-07-18 13:46:35 +0200

    Building a Login Page!!!

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RunGroupWebApp.Data;
+using RunGroupWebApp.Models;
+using RunGroupWebApp.ViewModels;
+
+namespace RunGroupWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDBContext _context;
+
+        public AccountController(UserManager<AppUser> userManager,SignInManager<AppUser> signInManager,ApplicationDBContext context)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View(response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid) return View(loginViewModel);
+
+            var user = await _userManager.FindByEmailAsync(loginViewModel.EmailAddress);
+            //login succes logic
+            if (user != null) 
+            {
+                var passwordChecking = await _userManager.CheckPasswordAsync(user,loginViewModel.Password);
+                if (passwordChecking)
+                {
+                    //password correct, sign in
+                    var result = 
+                        await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+                    if(result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Race");
+                    }
+                }
+                //login failed
+                TempData["Error"] = "Wrong credentials, Please try again!";
+                return View(loginViewModel);
+            }
+            //User not found
+            TempData["Error"] = "Wrong credentials, Please try again";
+            return View(loginViewModel);
+        }
+    }
+}
